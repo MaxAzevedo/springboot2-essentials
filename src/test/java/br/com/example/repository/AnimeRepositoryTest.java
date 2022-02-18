dac5296f8e9a97b91c8f228508cc4c8bc0710c14
max azevedo <max.azevedo@paygo-consult.com.br>
2022-02-18 01:55:15 -0300

    Teste unitários do repository

--- src/test/java/br/com/example/repository/AnimeRepositoryTest.java
+++ src/test/java/br/com/example/repository/AnimeRepositoryTest.java
@@ -15,12 +15,58 @@ class AnimeRepositoryTest {
     private AnimeRepository animeRepository;
 
     @Test
+    @DisplayName("Save anime when successful")
     void savePersistAnimeWhenSuccessful(){
         Anime anime = createAnime();
         Anime saved = this.animeRepository.save(anime);
         Assertions.assertTrue(saved != null);
         Assertions.assertTrue(saved.getId() != null);
         Assertions.assertTrue(saved.getName() == anime.getName());
+    }
+
+    @Test
+    @DisplayName("Update anime when successful")
+    void saveUpdateAnimeWhenSuccessful(){
+        var anime = createAnime();
+        var saved = this.animeRepository.save(anime);
+
+        saved.setName("Anime updated");
+        var updated = this.animeRepository.save(saved);
+
+        Assertions.assertTrue(saved != null);
+        Assertions.assertTrue(saved.getId() != null);
+        Assertions.assertEquals(saved.getName(), "Anime updated");
+    }
+
+    @Test
+    @DisplayName("Delete anime when successful")
+    void deleteAnimeWhenSuccessful(){
+        var anime = createAnime();
+        var saved = this.animeRepository.save(anime);
+        this.animeRepository.delete(saved);
+        var deleted = animeRepository.findById(saved.getId());
+        Assertions.assertTrue(deleted.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Find anime by name when successful")
+    void findByNameWhenSuccessful(){
+        var anime = createAnime();
+        this.animeRepository.save(anime);
+        var animes = animeRepository.findByName("Meninas Superpoderosas");
+        Assertions.assertTrue(!animes.isEmpty());
+        Assertions.assertTrue(animes.contains(anime));
+
+    }
+
+    @Test
+    @DisplayName("Find anime by name when not found")
+    void findByNameWhenNotFoundAnime(){
+        var anime = createAnime();
+        this.animeRepository.save(anime);
+        var animes = animeRepository.findByName("Invalid Anime");
+        Assertions.assertTrue(animes.isEmpty());
+
     }
 
     private Anime createAnime(){
